44ae9c02c00addd8455a932ea23febcc90f41729
LBohman <lovisabo@hotmail.com>
2023-02-28 17:48:50 +0100

    Add service implementations for character, movie and franchise

---
+++ assignment3/src/main/java/com/example/assignment3/services/MovieServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.assignment3.services;
+
+import java.util.Collection;
+
+public class MovieServiceImpl implements MovieService {
+    @Override
+    public Movie findById(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public Collection<Movie> findAll() {
+        return null;
+    }
+
+    @Override
+    public Movie add(Movie entity) {
+        return null;
+    }
+
+    @Override
+    public Movie update(Movie entity) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Integer integer) {
+
+    }
+}
