ae17436b0e864bdab524863b9812d62c3e90b00d
LBohman <lovisabo@hotmail.com>
2023-03-02 09:59:59 +0100

    Comment out code in AppRunner in favor for the controller, service and repository for movie. Might Remove all together later...

--- assignment3/src/main/java/com/example/assignment3/runners/AppRunner.java
+++ assignment3/src/main/java/com/example/assignment3/runners/AppRunner.java
@@ -6,11 +6,6 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-/**
- * Client class for the Services.
- * This class exists to demonstrate the use of services.
- * It implements ApplicationRunner to be able to use dependency injection.
- */
 @Component
 public class AppRunner implements ApplicationRunner {
     private final MovieRepository movieRepository;
@@ -21,8 +16,8 @@ public class AppRunner implements ApplicationRunner {
     @Override
     @Transactional
     public void run(ApplicationArguments args) throws Exception {
-        //movieService.deleteById(1);
-        System.out.println("Hello from runner");
-        System.out.println(movieRepository.findById(1).get().getCharacters());
+//        movieService.deleteById(1);
+//        System.out.println("Hello from runner");
+//        System.out.println(movieRepository.findById(1).get().getCharacters());
     }
 }
